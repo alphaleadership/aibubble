28a608e3d277986b7c9117f83ae1829d1f444caa
arbinger <alphaleadershipyt@protonmail.com>
2025-12-16 16:18:39 +0100

    patch

--- prebuild.cjs
+++ prebuild.cjs
@@ -1,5 +1,5 @@
 const comp=require("./companies.json")
 const fs=require("fs")
 comp.links=comp.links.map((link,index)=>({...link,id:`link-${index}`,name:`${link.source} to ${link.target}`,index}))
-comp.nodes=comp.nodes.map((node,index)=>({...node,name:node.company,index}))
+comp.nodes=comp.nodes.map((node,index)=>({...node,name:node.id,index}))
 fs.writeFileSync("./companies.json",JSON.stringify(comp,null,2))
