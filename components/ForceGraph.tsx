6826082ebab5d5a8b80d6185e19e7eeb80e6d2e5
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-12-16 09:18:05 +0100

    up

--- components/ForceGraph.tsx
+++ components/ForceGraph.tsx
@@ -59,7 +59,7 @@ const ForceGraph: React.FC<ForceGraphProps> = ({ data, onNodeClick, width, heigh
 
     // Deep copy data for D3
     const nodes = filteredNodes.map(d => ({ ...d })) as (NodeData & d3.SimulationNodeDatum)[];
-    const links = filteredLinks.map(d => ({ ...d })) as (LinkData & d3.SimulationLinkDatum<NodeData >)[];
+    const links = filteredLinks.map(d => ({ ...d })) as (LinkData & d3.SimulationLinkDatum)[];
 
     const svg = d3.select(svgRef.current)
       .attr("viewBox", [0, 0, width, height])
